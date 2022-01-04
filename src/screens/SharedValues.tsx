3e8d0c10ac2ffd352f27cf31e15654665865aaaf
Tijs Martens <tijs@bothrs.com>
2022-01-04 12:24:13 +0000

    feat: add spring options

--- src/screens/SharedValues.tsx
+++ src/screens/SharedValues.tsx
@@ -3,20 +3,26 @@ import Animated, {
   useAnimatedStyle,
   useSharedValue,
   withSpring,
+  WithSpringConfig,
 } from "react-native-reanimated";
 
 export function SharedValues() {
   const offsetX = useSharedValue(0);
   const offsetY = useSharedValue(0);
 
+  const springOptions: WithSpringConfig = {
+    damping: 20, // jumpyness -> lower is more jumpy
+    mass: 1, // -> speed - higher = faster
+  };
+
   const animatedStyles = useAnimatedStyle(() => {
     return {
       transform: [
         {
-          translateX: withSpring(offsetX.value * 200),
+          translateX: withSpring(offsetX.value * 200, springOptions),
         },
         {
-          translateY: withSpring(offsetY.value * 300),
+          translateY: withSpring(offsetY.value * 300, springOptions),
         },
       ],
     };
